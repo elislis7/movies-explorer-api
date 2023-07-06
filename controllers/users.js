1e65a09a2295d0cd52ca9002cf0cbd3058f9ee1a
Elizabeth Lis <elislis7@ya.ru>
2023-07-06 12:24:54 +0300

    добавлен файл .evn, отредактирован .gitignore и readme

--- controllers/users.js
+++ controllers/users.js
@@ -87,7 +87,7 @@ const login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'JWT-token', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
