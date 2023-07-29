a1653322e52a1c2bf3b7429564ac0a1eef384c2e
Elizabeth Lis <elislis7@ya.ru>
2023-07-29 16:16:34 +0300

    исправлена ошибка lintera

--- app.js
+++ app.js
@@ -8,16 +8,22 @@ const limiter = require('./middlewares/rateLimiter');
 
 const router = require('./routes/index');
 const handelError = require('./middlewares/handelError');
-const baseUrl = require('./utils/config');
+/* const baseUrl = require('./utils/config'); */
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
-const { NODE_ENV, DB_CONNECT } = process.env;
+const { PORT = 3001 } = process.env;
+const { DB_CONNECT = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(helmet());
 app.use(express.json());
 
@@ -31,8 +37,9 @@ app.use(errorLogger);
 app.use(errors());
 app.use(handelError);
 
-mongoose.connect(NODE_ENV === 'production' ? DB_CONNECT : baseUrl);
+mongoose.connect(DB_CONNECT);
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
--- models/user.js
+++ models/user.js
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-userSchema.statics.findUserByCredentials = function (email, password) {
+userSchema.statics.findUserByCredentials = function findOne(email, password) {
   return this
     .findOne({ email })
     .select('+password')
